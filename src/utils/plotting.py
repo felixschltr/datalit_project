d384fa9682e412a02a7474d9740869269181be3d
felixschltr <f.schlueter@tuta.io>
2022-02-03 15:18:00 +0100

    add line plots

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,14 +1,17 @@
+
+from turtle import color
+from typing import List
 import numpy as np
 import pandas as pd
 import cycler
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
+from tueplots import bundles
 from .summarize import get_diffs, get_means
 
+# tueplots settings
+# mpl.rcParams.update(bundles.neurips2021(usetex=False))
 # color settings
-n = 20
-#color = plt.cm.tab20(np.linspace(0,1,n))
 mpl.rcParams['axes.prop_cycle'] = cycler.cycler('color', plt.cm.tab20.colors)
 
 def plot_means_vs_diffs(
@@ -47,12 +50,13 @@ def plot_means_vs_diffs(
             fontsize = kwargs.get('text_fontsize', 9)
             )
     # add correlation annotation
-    ax.text(
-        x_corr + 0.05,
-        y_corr - 0.05,
-        f'Pearson $r$ = {round(pearson_r, 3)}',
-        fontsize = 12
-        )
+    if kwargs.get('correl', True):
+        ax.text(
+            x_corr + 0.05,
+            y_corr - 0.05,
+            f'Pearson $r$ = {round(pearson_r, 3)}',
+            fontsize = 12
+            )
     ax.set_title(
         kwargs.get('title', 'RD&D spending vs. share of energy from renewable sources')
     )
@@ -106,3 +110,197 @@ def plot_rdd(rdd_data : pd.DataFrame, **kwargs):
     ax.legend(bbox_to_anchor=(1.0,1.0), loc='upper left')
     ax.grid(axis=kwargs.get('gridaxis', 'y'))
     ax.set_axisbelow(True)
+
+def plot_lines(
+    rdd_data : pd.DataFrame,
+    nrg_data : pd.DataFrame,
+    t_window = 10,
+    y_shift = 10,
+    avg_window = 1,
+    **kwargs
+    ):
+    fig, ax1 = plt.subplots(figsize=kwargs.get('figsize', (10,8)))
+    ax2 = ax1.twiny() # create twin y axis
+    # ax1 for nrg_data
+    ymax1 = kwargs.get('ymax1',max(nrg_data['year'].unique()))
+    ymin1 = ymax1 - t_window
+    assert ymin1 >= min(nrg_data['year'].unique()), \
+        f"bounds of available data exceeded!"
+    nrg_data = nrg_data[nrg_data['year'].isin(range(ymin1, ymax1+1))]
+    # ax2 for rdd_data
+    ymax2 = ymax1 - y_shift
+    ymin2 = ymax2 - t_window
+    assert ymin2 >= min(rdd_data['year'].unique()), \
+        f"bounds of available data exceeded!"
+    rdd_data = rdd_data[rdd_data['year'].isin(range(ymin2, ymax2+1))]
+    ###
+    for country in kwargs.get('selected', rdd_data['country'].unique()):
+        # plot nrg_data
+        label1 = 'share of primary\nenergy from renewable\nsources'
+        sub_data = (
+            nrg_data[(nrg_data['country'] == country)]
+            .sort_values(by='year')
+        )
+        x1 = sub_data['year']
+        y1 = sub_data['prim_nrg_share_renewables']
+        ax1.plot(x1, y1, '-s', c='C0', label=label1)
+        # ax1 formatting
+        x_ticks = [int(y) for y in x1]
+        ax1.set_xticks(x_ticks)
+        ax1.set_xticklabels(x_ticks, rotation=45)
+        # plot rdd_data
+        sub_data = (
+            rdd_data[(rdd_data['country'] == country)]
+            .sort_values(by='year')
+        )
+        x2 = sub_data['year']
+        y2 = sub_data['share_budget_renewables']
+        #avg_window = kwargs.get('avg_window', 1)
+        y2_avg = y2.rolling(window=avg_window).mean()
+        # plot non-averaged data
+        label2 = 'share of RD&D\nspending on renewable\nenergy technology'
+        if avg_window != 1:
+            label2 += ' (averaged)'
+        if kwargs.get('plot_non_avg', False):
+            ax2.plot(x2, y2, ls='--',alpha=kwargs.get('alpha', 0.6), c='C2', label='share RD&D non-average')
+        # plot averaged data
+        ax2.plot(x2, y2_avg, '-o', c='C2', label=label2)
+        # ax2 formatting
+        x_ticks = [int(year) for year in x2]
+        ax2.set_xticks(x_ticks)
+        ax2.set_xticklabels(x_ticks, rotation=45)
+        # add correlation annotation
+        if kwargs.get('correl', False):
+            #get correlation
+            pearson_r = np.corrcoef(y1,y2)[0][1]
+            x_corr = min(x1)
+            y_corr = max(max(y1), max(y2_avg))
+            ax1.text(
+                x_corr + 0.05,
+                y_corr - 0.05,
+                f'Pearson $r$ = {round(pearson_r, 3)}',
+                fontsize = 12
+                )
+    # global formatting
+    default_title = (
+        'Share of RD&D budged invested in renewable energy sources\nvs.'+
+        '\nShare of renewables in primary energy\ntime shifted'+
+        f'by {y_shift} years'
+    )
+    ax1.set_xlabel(f'Time for {label1}')
+    ax2.set_xlabel(f'Time for {label2}')
+    ax1.set_ylabel('%') # global y-axis label
+    ax1.set_title(kwargs.get('title', default_title))
+    lin1, lab1 = ax1.get_legend_handles_labels()
+    lin2, lab2, = ax2.get_legend_handles_labels()
+    ax1.legend(lin1 + lin2, lab1 +lab2,bbox_to_anchor=(1.0,1.0), loc='upper left')
+    ax1.grid(axis=kwargs.get('gridaxsis', 'y'))
+    ax1.set_axisbelow(True)
+
+def get_mean_std(data : pd.DataFrame, selected : List[str], coi : str) -> pd.DataFrame:
+    """
+    Returns per-year means and (corrected) standard deviations of values
+    in column of interest over selected countries
+
+    Parameters
+    ----------
+    data : data frame containing data sorted by year and country
+    selected : list of countries to compute means and std.s for
+    coi : name of column of interest, i.e. column to compute statistics for
+
+    Returns
+    -------
+    merged data frame containing mean and (corrected) st.dev. per year
+    """
+    data = data[data['country'].isin(selected)]
+    mean_df = (
+        data.groupby('year').mean()
+        .reset_index()
+        [['year', coi]]
+    )
+    std_df = (
+        data.groupby('year').std(ddof=1)
+        .reset_index()
+        [['year', coi]]
+    )
+    # merge into single data frame
+    merged_df = mean_df.merge(std_df, how='inner', on='year')
+    # rename columns
+    merged_df.rename(columns={f'{coi}_x':'mean', f'{coi}_y':'std'}, inplace=True)
+    return merged_df
+
+def plot_lines_global(
+    rdd_data,
+    nrg_data,
+    t_window : int = 10,
+    y_shift : int = 10,
+    **kwargs
+    ):
+    # formatting of error bars
+    elinewidt = kwargs.get('elinewidt', 1)
+    capsize = kwargs.get('capsize', 2)
+    # select countries
+    selected = kwargs.get('selected', rdd_data['country'].unique())
+    ## get statistics for rdd_data
+    rdd_coi = kwargs.get('rdd_coi', 'share_budget_renewables')
+    rdd_data = get_mean_std(rdd_data, selected, rdd_coi)
+    ## get statistics for nrg_data
+    nrg_coi = kwargs.get('nrg_coi','prim_nrg_share_renewables')
+    nrg_data = get_mean_std(nrg_data, selected, nrg_coi)
+    ## acutal plotting
+    fig, ax1 = plt.subplots(figsize=kwargs.get('figsize', (10,8)))
+    ax2 = ax1.twiny() # create twin y axis
+    # ax1 for nrg_data
+    label1 = 'share of primary\nenergy from renewable\nsources'
+    ymax1 = kwargs.get('ymax1',max(nrg_data['year'].unique()))
+    ymin1 = ymax1 - t_window
+    assert ymin1 >= min(nrg_data['year'].unique()), \
+        f"bounds of available data exceeded!"
+    nrg_data = nrg_data[nrg_data['year'].isin(range(ymin1, ymax1+1))]
+    x1 = nrg_data['year']
+    y1 = nrg_data['mean']
+    y1err = nrg_data['std']
+    ax1.errorbar(
+        x1, y1, y1err, fmt='-s', elinewidth=elinewidt,
+        capsize=capsize, color='C0', label=label1)
+    # ax1 formatting
+    x_ticks = [int(y) for y in x1]
+    ax1.set_xticks(x_ticks)
+    ax1.set_xticklabels(x_ticks, rotation=45)
+    # ax2 for rdd_data
+    label2 = 'share of RD&D\nspending on renewable\nenergy technology'
+    ymax2 = ymax1 - y_shift
+    ymin2 = ymax2 - t_window
+    assert ymin2 >= min(rdd_data['year'].unique()), \
+        f"bounds of available data exceeded!"
+    rdd_data = rdd_data[rdd_data['year'].isin(range(ymin2, ymax2+1))]
+    x2 = rdd_data['year']
+    y2 = rdd_data['mean']
+    y2err = rdd_data['std']
+    ax2.errorbar(
+        x2, y2, y2err, fmt='-o', elinewidth=elinewidt,
+        capsize=capsize, color='C2', label=label2)
+    # ax2 formatting
+    x_ticks = [int(y) for y in x2]
+    ax2.set_xticks(x_ticks)
+    ax2.set_xticklabels(x_ticks, rotation=45)
+    # fill error
+    if kwargs.get('fill_error', False):
+        ax1.fill_between(x1, y1+y1err,y1-y1err, color='C0', alpha=0.1)
+        ax2.fill_between(x2, y2+y2err,y2-y2err, color='C2', alpha=0.1)
+    # global formatting
+    default_title = (
+        'Share of RD&D budged invested in renewable energy sources\nvs.'+
+        '\nShare of renewables in primary energy\ntime shifted'+
+        f'by {y_shift} years'
+    )
+    ax1.set_xlabel(f'Time for {label1}')
+    ax2.set_xlabel(f'Time for {label2}')
+    ax1.set_ylabel('%') # global y-axis label
+    ax1.set_title(kwargs.get('title', default_title))
+    lin1, lab1 = ax1.get_legend_handles_labels()
+    lin2, lab2, = ax2.get_legend_handles_labels()
+    ax1.legend(lin1 + lin2, lab1 +lab2,bbox_to_anchor=(1.0,1.0), loc='upper left')
+    ax1.grid(axis=kwargs.get('gridaxsis', 'y'))
+    ax1.set_axisbelow(True)
+
